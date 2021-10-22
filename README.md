afe72480f7a1e1f7b6919ff4117ee345bb8ef79d
browndddddd <browndddddd@gmail.com>
2021-10-22 16:46:09 +0700

    splitting 'assets' folder structure and done shared preferences for onboarding screen

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # grow_app
 A friend will collaborate and help us manage projects, tasks, time,...
+
+<!-- export PATH="$PATH:/Users/Brown.D/Developer/flutter/bin" -->
---
+++ lib/screens/signin.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class signinScreen extends StatelessWidget {
+  const signinScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sign In Screen'),
+      ),
+    );
+  }
+}
