2d7e50b223de8dbbb7444844169186a145e1a5e8
browndddddd <browndddddd@gmail.com>
2021-10-27 22:55:50 +0700

    done terms & conditions screen

--- lib/constants/fonts.dart
+++ lib/constants/fonts.dart
@@ -5,6 +5,7 @@ import 'package:flutter/cupertino.dart';
 
 const double title32 = 32.0;
 const double title28 = 28.0;
+const double title18 = 18.0;
 const double content14 = 14.0;
 const double content16 = 16.0;
 const double suggestion12 = 12.0;
